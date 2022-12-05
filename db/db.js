1e27449f5132c5bf104f5144dfaba891f61771c6
Gvbmaster <gnangndong@gaming.tech>
2022-12-05 19:08:39 +0100

    NodeJS

---
+++ db/db.js
@@ -0,0 +1,28 @@
+const { MongoClient } = require("mongodb");
+const connectionString =
+  "mongodb+srv://Gvbmaster:QOwqS4KhrUROKmgp@cluster0.wyeljsm.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let dbConnection;
+
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      if (err || !db) {
+        return err;
+      }
+      
+      //remplacer whatever par le nom de votre DB !
+      dbConnection = db.db("pokemon");
+      console.log("Successfully connected to MongoDB.");
+    });
+    
+  },
+
+  getDb: function () {
+    return dbConnection;
+  },
+};
